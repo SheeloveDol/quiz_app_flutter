fa1172563e291b702912f08aee54474a92beca00
Sheelove Dol <siesel26@gmail.com>
2024-03-11 13:52:42 -0700

    Added logo with title and outlinedButton

--- lib/background_container.dart
+++ lib/background_container.dart
@@ -5,6 +5,28 @@ class BackgroundContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.purple[900]);
+    return Container(
+      color: Colors.purple[900],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Image(
+              image: AssetImage('assets/images/quiz-logo.png'),
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 50),
+            Text('Learn Flutter the fun way!',
+                style: TextStyle(fontSize: 20, color: Colors.purple[100])),
+            const SizedBox(height: 50),
+            OutlinedButton(
+                onPressed: () => {},
+                child: Text('Start Quiz',
+                    style: TextStyle(color: Colors.purple[100])))
+          ],
+        ),
+      ),
+    );
   }
 }
