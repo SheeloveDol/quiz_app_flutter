61e5726d35e327e2be8565fa5bd6de5ef3291ef6
Sheelove Dol <siesel26@gmail.com>
2024-03-11 13:01:48 -0700

    Created main page and added background_container component

---
+++ lib/background_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BackgroundContainer extends StatelessWidget {
+  const BackgroundContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.purple[900]);
+  }
+}
