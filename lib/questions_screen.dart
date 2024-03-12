de3eb5c7d85bcd396dceb54388b651daa7d0bed5
Sheelove Dol <siesel26@gmail.com>
2024-03-12 16:51:35 -0700

    Created custom answer_button widget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,34 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('This is the questions screen.');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question 1'),
+          const SizedBox(height: 20),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
+          ), // When using named arguments, you must first specify the name of the argument
+          const SizedBox(height: 20),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
+          ), // When using named arguments, you must first specify the name of the argument
+          const SizedBox(height: 20),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
+          ), // When using named arguments, you must first specify the name of the argument
+          const SizedBox(height: 20),
+          AnswerButton(
+            answerText: 'Answer 4',
+            onTap: () {},
+          ), // When using named arguments, you must first specify the name of the argument
+        ],
+      ),
+    );
   }
 }
