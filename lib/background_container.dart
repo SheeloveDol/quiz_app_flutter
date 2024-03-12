5a7f36984d1856a5236bd156689732e535812806
Sheelove Dol <siesel26@gmail.com>
2024-03-11 17:22:23 -0700

    Added right arrow to button and refactored layout of logo

--- lib/background_container.dart
+++ lib/background_container.dart
@@ -6,23 +6,44 @@ class BackgroundContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.purple[900],
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(254, 29, 4, 82),
+            Color.fromARGB(254, 68, 10, 80),
+          ],
+        ),
+      ),
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+
+          // A better/different way of doing it
+          mainAxisSize: MainAxisSize
+              .min, // This will make the column to take the minimum space required
           children: <Widget>[
-            const Image(
-              image: AssetImage('assets/images/quiz-logo.png'),
-              width: double.infinity,
-              fit: BoxFit.cover,
+            // const Image(
+            //   image: AssetImage('assets/images/quiz-logo.png'),
+            //   width: double.infinity,
+            //   fit: BoxFit.cover,
+            // ),
+
+            // A better way of doing it
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
             ),
             const SizedBox(height: 50),
             Text('Learn Flutter the fun way!',
                 style: TextStyle(fontSize: 20, color: Colors.purple[100])),
             const SizedBox(height: 50),
-            OutlinedButton(
+            OutlinedButton.icon(
                 onPressed: () => {},
-                child: Text('Start Quiz',
+                icon: Icon(Icons.arrow_right_alt_outlined,
+                    color: Colors.purple[100]),
+                label: Text('Start Quiz',
                     style: TextStyle(color: Colors.purple[100])))
           ],
         ),
