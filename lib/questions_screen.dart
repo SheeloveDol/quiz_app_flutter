4d058f7463de286d760e25e3ffea19e3af3031a2
Sheelove Dol <siesel26@gmail.com>
2024-03-12 22:13:48 -0700

    Refactored questions_screen.dart to map over list of answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -11,35 +12,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    // to iterate through the list of answers we first declare a variable to hold the class instance
+    final currentQuestion =
+        questions[0]; // <-- This is the first question in the list
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'Question 1',
-            style: TextStyle(fontSize: 20, color: Colors.purple[100]),
+            currentQuestion
+                .question, // <-- This is the text of the first question
+            style: TextStyle(fontSize: 16, color: Colors.purple[100]),
           ),
           const SizedBox(height: 40),
-          AnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {},
-          ), // When using named arguments, you must first specify the name of the argument
-          const SizedBox(height: 20),
-          AnswerButton(
-            answerText: 'Answer 2',
-            onTap: () {},
-          ), // When using named arguments, you must first specify the name of the argument
-          const SizedBox(height: 20),
-          AnswerButton(
-            answerText: 'Answer 3',
-            onTap: () {},
-          ), // When using named arguments, you must first specify the name of the argument
-          const SizedBox(height: 20),
-          AnswerButton(
-            answerText: 'Answer 4',
-            onTap: () {},
-          ), // When using named arguments, you must first specify the name of the argument
+          ...currentQuestion.answers.map((answer) {
+            // <-- This is the list of answers and uses the spread operator and map to iterate through the list
+            return AnswerButton(answerText: answer, onTap: () {});
+          })
         ],
       ),
     );
