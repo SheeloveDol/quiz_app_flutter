ebfb77a25b042368c7200b9e7336e2e65cb9c143
Sheelove Dol <siesel26@gmail.com>
2024-03-14 13:36:38 -0700

    Wrapped Column inside a SingleChildScrollView and a SizedBox with defined height

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,36 +7,43 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(
-            children: [
-              Text(((data['question_index'] as int) + 1)
-                  .toString()), // The Text widget takes a string, so convert the index to a string.
-              Expanded(
-                child: Column(children: [
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
                   Text(
-                    data['question'] as String,
+                    ((data['question_index'] as int) + 1).toString(),
                     style: const TextStyle(color: Colors.white),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    data['correct_answer'] as String,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    data['chosen_answer'] as String,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  const SizedBox(height: 20),
-                ]),
-              )
-            ],
-          );
-        },
-      ).toList(), // The column widget takes a list of widgets as children, so convert the list of maps to a list of widgets.
+                  ), // The Text widget takes a string, so convert the index to a string.
+                  Expanded(
+                    child: Column(children: [
+                      Text(
+                        data['question'] as String,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        data['chosen_answer'] as String,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(height: 20),
+                    ]),
+                  )
+                ],
+              );
+            },
+          ).toList(), // The column widget takes a list of widgets as children, so convert the list of maps to a list of widgets.
+        ),
+      ),
     );
   }
 }
